7af93a5b6cc279170dd40d8c3112a4eaa5d53781
Juan Ignacio <jaimovich.ignacio@gmail.com>
2022-12-17 13:25:22 -0300

    static img

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static("public"));
+
 app.use( '/api/products', productsRouter );
 app.use( '/api/carts', cartsRouter );
 
